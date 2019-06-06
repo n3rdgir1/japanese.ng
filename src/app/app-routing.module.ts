a7fdfb1a5871ef610f4284f6f159504b9e587405
n3rdgir1 <nerdgirl11235813@gmail.com>
2019-06-05 19:28:53 -0500

    moves quiz to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { QuizComponent } from './quiz/quiz/quiz.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 const routes: Routes = [
   {path: '', component: QuizComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuizComponent } from './quiz/quiz/quiz.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
