f7e8c7572cd43f492585e34a6764b10a1b3e84de
n3rdgir1 <nerdgirl11235813@gmail.com>
2019-06-07 16:02:04 -0500

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { QuizComponent } from './components/quiz/quiz.component';
 import { KeyComponent } from './components/key/key.component';
 
 const routes: Routes = [
-  {path: '', component: QuizComponent},
-  {path: 'key', component: KeyComponent}
+  {path: 'quiz', component: QuizComponent},
+  {path: 'key', component: KeyComponent},
+  {path: '**', component: QuizComponent}
 ];
 
 @NgModule({
